aed250e0bdacd0278eb54e0b4d448fb78e4118ad
Samin Shahriar Tokey <saminshahriar@rocketmail.com>
2022-04-04 13:59:39 +0600

    Stash

--- Source/Zombiefield/ZombiefieldCharacter.cpp
+++ Source/Zombiefield/ZombiefieldCharacter.cpp
@@ -66,37 +66,10 @@ AZombiefieldCharacter::AZombiefieldCharacter()
 
 	FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
 	FP_MuzzleLocation->SetupAttachment(FP_Gun);
-	FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 2.4f, -26666622.6f));
+	FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 2.4f, -10.6f));
 
 	// Default offset from the character location for projectiles to spawn
 	GunOffset = FVector(100.0f, 0.0f, 10.0f);
-
-	// Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P, FP_Gun, and VR_Gun 
-	// are set in the derived blueprint asset named MyCharacter to avoid direct content references in C++.
-
-	// Create VR Controllers.
-	R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));
-	R_MotionController->MotionSource = FXRMotionControllerBase::RightHandSourceId;
-	R_MotionController->SetupAttachment(RootComponent);
-	L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
-	L_MotionController->SetupAttachment(RootComponent);
-
-	// Create a gun and attach it to the right-hand VR controller.
-	// Create a gun mesh component
-	VR_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VR_Gun"));
-	VR_Gun->SetOnlyOwnerSee(false);			// otherwise won't be visible in the multiplayer
-	VR_Gun->bCastDynamicShadow = false;
-	VR_Gun->CastShadow = false;
-	VR_Gun->SetupAttachment(R_MotionController);
-	VR_Gun->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
-
-	VR_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("VR_MuzzleLocation"));
-	VR_MuzzleLocation->SetupAttachment(VR_Gun);
-	VR_MuzzleLocation->SetRelativeLocation(FVector(0.000004, 53.999992, 10.000000));
-	VR_MuzzleLocation->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f));		// Counteract the rotation of the VR gun model.
-
-	// Uncomment the following line to turn motion controllers on by default:
-	//bUsingMotionControllers = true;
 
 	PrimaryActorTick.bCanEverTick = true;
 }
@@ -108,18 +81,6 @@ void AZombiefieldCharacter::BeginPlay()
 
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
-
-	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
-	if (bUsingMotionControllers)
-	{
-		VR_Gun->SetHiddenInGame(false, true);
-		Mesh1P->SetHiddenInGame(true, true);
-	}
-	else
-	{
-		VR_Gun->SetHiddenInGame(true, true);
-		Mesh1P->SetHiddenInGame(false, true);
-	}
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -135,10 +96,6 @@ void AZombiefieldCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &AZombiefieldCharacter::Sprint);
 	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &AZombiefieldCharacter::StopSprint);
-
-	// Bind fire event
-
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AZombiefieldCharacter::OnResetVR);
 
 	// Bind movement events
 	PlayerInputComponent->BindAxis("MoveForward", this, &AZombiefieldCharacter::MoveForward);
@@ -156,7 +113,7 @@ void AZombiefieldCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 
 void AZombiefieldCharacter::OnFire(float firePressed)
 {
-	
+
 	if (firePressed == 1.0)
 	{
 		fired -= deltaTime;
@@ -170,28 +127,18 @@ void AZombiefieldCharacter::OnFire(float firePressed)
 				UWorld* const World = GetWorld();
 				if (World != nullptr)
 				{
-					if (bUsingMotionControllers)
-					{
-						const FRotator SpawnRotation = VR_MuzzleLocation->GetComponentRotation();
-						const FVector SpawnLocation = VR_MuzzleLocation->GetComponentLocation();
-						World->SpawnActor<AZombiefieldProjectile>(ProjectileClass, SpawnLocation, SpawnRotation);
-					}
-					else
-					{
-						const FRotator SpawnRotation = GetControlRotation();
-						// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
-						const FVector SpawnLocation = ((FP_MuzzleLocation != nullptr) ? FP_MuzzleLocation->GetComponentLocation() : GetActorLocation()) + SpawnRotation.RotateVector(GunOffset);
+					const FRotator SpawnRotation = GetControlRotation();
+					// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+					const FVector SpawnLocation = ((FP_MuzzleLocation != nullptr) ? FP_MuzzleLocation->GetComponentLocation() : GetActorLocation()) + SpawnRotation.RotateVector(GunOffset);
 
-						//Set Spawn Collision Handling Override
-						FActorSpawnParameters ActorSpawnParams;
-						ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+					//Set Spawn Collision Handling Override
+					FActorSpawnParameters ActorSpawnParams;
+					ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
 
-						// spawn the projectile at the muzzle
-						World->SpawnActor<AZombiefieldProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
-					}
+					// spawn the projectile at the muzzle
+					World->SpawnActor<AZombiefieldProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
 				}
 			}
-
 
 			// try and play the sound if specified
 			if (FireSound != nullptr)
@@ -213,11 +160,6 @@ void AZombiefieldCharacter::OnFire(float firePressed)
 	}
 	else if (firePressed == 0.0)
 		fired = 0;
-}
-
-void AZombiefieldCharacter::OnResetVR()
-{
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
 }
 
 
@@ -270,5 +212,9 @@ void AZombiefieldCharacter::Tick(float DeltaTime)
 	deltaTime = DeltaTime;
 	const FRotator SpawnRotation = GetControlRotation();
 	const FVector SpawnLocation = ((FP_MuzzleLocation != nullptr) ? FP_MuzzleLocation->GetComponentLocation() : GetActorLocation()) + SpawnRotation.RotateVector(GunOffset);
-	DrawDebugLine(GetWorld(), SpawnLocation + SpawnRotation.Vector()*-90, SpawnLocation + SpawnRotation.Vector() * 9000, FColor::Red, false, 0.0f,0,1.9f);
+	FActorSpawnParameters ActorSpawnParams;
+	ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+	GetWorld()->SpawnActor<AZombiefieldProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+	GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::White, FString::Printf(TEXT("WalkSpeed %f"), SpawnLocation.Y));
+	DrawDebugLine(GetWorld(), SpawnLocation + SpawnRotation.Vector() * -90, SpawnLocation + SpawnRotation.Vector() * 9000, FColor::Red, false, 0.1f, 0, 1.9f);
 }
